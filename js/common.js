e1ed36e4aa24131a0e1aebf9f67204be9c492f7e
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-08-23 08:41:18 +0600

    display player name function add

--- js/common.js
+++ js/common.js
@@ -19,3 +19,33 @@ const playerArray = [];
 
 
 
+function displayPlayerName(PlayerName) {
+  const listOfPlayerName = document.getElementById("list-of-player-name");
+  listOfPlayerName.innerText = "";
+  
+  for (let i = 0; i < PlayerName.length; i++) {
+   if ([i] <= 4) {
+     const playerNames = playerArray[i].playerName;
+     
+     const tr = document.createElement("tr");
+  
+     tr.innerHTML = `   
+       
+        <th class="fs-4 fw-semibold ">${i + 1}</th> 
+        <td class="fs-4 fw-semibold "> ${playerNames}</td> 
+        `;
+        listOfPlayerName.appendChild(tr);
+        
+   } else {
+     alert("you can select maximum 5 player");
+     
+     return;
+   }
+  }
+  }
+  
+  
+  
+  
+  
+  
