925d0c766e411206c1a28b7048c071cdec5e9ed5
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-08-23 12:51:26 +0600

    All problem solve and final added

--- js/common.js
+++ js/common.js
@@ -1,3 +1,28 @@
+const playerArray = [];
+
+function displayPlayerName(PlayerName) {
+  const listOfPlayerName = document.getElementById("list-of-player-name");
+  listOfPlayerName.innerText = "";
+
+  for (let i = 0; i < PlayerName.length; i++) {
+    const playerNames = playerArray[i].playerName;
+
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `   
+       
+        <th class="fs-4 fw-semibold ">${i + 1}</th> 
+        <td class="fs-4 fw-semibold "> ${playerNames}</td> 
+        `;
+    listOfPlayerName.appendChild(tr);
+  }
+};
+
+
+
+
+
+
 function buttonSelect(element) {
   const playerName = element.parentNode.children[0].innerText;
 
@@ -5,92 +30,64 @@ function buttonSelect(element) {
     playerName: playerName,
   };
 
-  playerArray.push(players);
+  if (playerName.length >= 5) {
+    element.setAttribute("disabled", true);
+    playerArray.push(players);
+    displayPlayerName(playerArray);
 
-  displayPlayerName(playerArray);
-
-  element.setAttribute("disabled", true);
-}
-
-const playerArray = [];
-
-
-
-
-
-
-function displayPlayerName(PlayerName) {
-  const listOfPlayerName = document.getElementById("list-of-player-name");
-  listOfPlayerName.innerText = "";
-  
-  for (let i = 0; i < PlayerName.length; i++) {
-   if ([i] <= 4) {
-     const playerNames = playerArray[i].playerName;
-     
-     const tr = document.createElement("tr");
-  
-     tr.innerHTML = `   
-       
-        <th class="fs-4 fw-semibold ">${i + 1}</th> 
-        <td class="fs-4 fw-semibold "> ${playerNames}</td> 
-        `;
-        listOfPlayerName.appendChild(tr);
-        
-   } else {
-     alert("you can select maximum 5 player");
-     
-     return;
-   }
+  } else {
+    alert("you can select maximum five player");
   }
-  }
-  
-  
-  
-  
-  
-  
-  document.getElementById("button-calculate").addEventListener("click", function () {
+};
+
+
+
+
+
+
+document
+  .getElementById("button-calculate")
+  .addEventListener("click", function () {
     const perPlayerInputField = document.getElementById("player-input-field");
     const perPlayerInputString = perPlayerInputField.value;
     const perPlayerInputValue = parseFloat(perPlayerInputString);
     if (isNaN(perPlayerInputValue)) {
-     alert("Please enter a valid number");
-     return;
+      alert("Please enter a valid number");
+      return;
     }
     const playerArrayLength = playerArray.length;
-    
+
     const perPlayerCost = playerArrayLength * perPlayerInputValue;
     const playerCostNumber = document.getElementById("player-cost");
     playerCostNumber.innerText = perPlayerCost;
-    });
-    
-    
-    
-    
-    
-    
-    document.getElementById("calculate-total").addEventListener("click", function () {
-      const playerCostNumber = document.getElementById("player-cost");
-      const playerCostString = playerCostNumber.innerText;
-      const playerCost= parseFloat(playerCostString);
-     
-      const managerInputField = document.getElementById("manager-input-field");
-      const managerInputString = managerInputField.value;
-      const managerInputValue = parseFloat(managerInputString);
-     
-      const coachInputField = document.getElementById("coach-input-field");
-      const coachInputString = coachInputField.value;
-      const coachInputValue = parseFloat(coachInputString);
-     
-      if (isNaN(managerInputValue) || isNaN(coachInputValue)) {
-        alert("Please enter the valid number");
-        return;
-      }
-     
-      const calculateTotal = playerCost + managerInputValue + coachInputValue;
-     
-      const totalAmount = document.getElementById("total-amount");
-     
-      totalAmount.innerText = calculateTotal;
-     });
-    
+  });
+
+
+
+
+
+
+document.getElementById("calculate-total") .addEventListener("click", function () {
+    const playerCostNumber = document.getElementById("player-cost");
+    const playerCostString = playerCostNumber.innerText;
+    const playerCost = parseFloat(playerCostString);
+
+    const managerInputField = document.getElementById("manager-input-field");
+    const managerInputString = managerInputField.value;
+    const managerInputValue = parseFloat(managerInputString);
+
+    const coachInputField = document.getElementById("coach-input-field");
+    const coachInputString = coachInputField.value;
+    const coachInputValue = parseFloat(coachInputString);
+
+    if (isNaN(managerInputValue) || isNaN(coachInputValue)) {
+      alert("Please enter the valid number");
+      return;
+    }
+
+    const calculateTotal = playerCost + managerInputValue + coachInputValue;
+
+    const totalAmount = document.getElementById("total-amount");
+
+    totalAmount.innerText = calculateTotal;
+  });
